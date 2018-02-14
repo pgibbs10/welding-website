c0769b9e50c894df6e96c1afaa03e71adc41b851
Paul Gibbs <pwgibbs10@gmail.com>
2018-02-14 14:43:46 -0700

    Updated things

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,7 @@ module WeldingWebsite
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
